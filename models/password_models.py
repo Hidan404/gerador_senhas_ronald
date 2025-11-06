e7310844b29ebd03df41fc2533a5f8878acf22a6
Ronald <ronaldkurouzo@gmail.com>
2025-11-06 20:56:32 -0300

    feat(passwordmodel): adicionar propriedades da classe e metodo de geração da senha
    implementando o metodo de gerar senhas do models
    
    BREAKING CHANGE:

--- models/password_models.py
+++ models/password_models.py
@@ -1,7 +1,17 @@
 import string
 import random
+import secrets
 
 
 class PasswordModel():
-    def __init__(self):
-        
+    def __init__(self,maiusculas=True,minusculas=True):
+        self.comprimento = int(input("Quantos caracteres: "))
+        self.maiusculas = maiusculas
+        self.minusculas = minusculas
+        self.caracteres = list(string.ascii_letters)
+
+
+
+
+senha = PasswordModel()
+print(senha.caracteres)        
