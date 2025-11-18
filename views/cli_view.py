b1e3aacfea31aa1f021e316bd946cb40e19c2e6f
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2025-11-17 21:09:54 -0300

    feat(cli_view): implementa a visualização CLI com lógica e inicialização do aplicativo principal feat(init): adicione __init__.py para marcar o diretório do pacote correção (requisitos): limpe dependências desnecessárias de requisitos.txt

--- views/cli_view.py
+++ views/cli_view.py
@@ -1,1 +1,12 @@
 from controllers.senha_controller import ExecutarControllers
+from models.password_models import SenhaModel
+
+
+
+class CliView():
+    def __init__(self):
+        self.controle = ExecutarControllers()
+        self.senha_model = SenhaModel()
+
+    def app(self):
+        while
